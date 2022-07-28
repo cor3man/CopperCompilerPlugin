83a9ebc12362d6ffabd108404cdc7197b19ccbac
cor3man <lisockij@gmail.com>
2022-07-28 13:40:50 +0200

    Creating directory and unified names

--- src/main/scala/com/copper/testplugin/CCPlugin.scala
+++ src/main/scala/com/copper/testplugin/CCPlugin.scala
@@ -1,16 +1,17 @@
 package com.copper.testplugin
 
-import com.copper.testplugin.components.BlockTreesGuardian
+import com.copper.testplugin.components.BlockTrees
 import com.copper.testplugin.model.Struct
+
 import java.io.{BufferedWriter, File, FileWriter}
 import java.util.Properties
 import scala.tools.nsc.{Global, Phase}
 import scala.tools.nsc.plugins.{Plugin, PluginComponent}
 
-class GuardedBlocksPlugin(val global: Global) extends Plugin {
+class CCPlugin(val global: Global) extends Plugin {
 
-  override val name: String = "guardedblocks"
-  override val description: String = "GuardedBlocks compiler plugin"
+  override val name: String = "ccplugin"
+  override val description: String = "Copper compiler plugin"
   override val components: List[PluginComponent] = List(new MyComponent(global))
 
   override def init(options: List[String], error: String => Unit): Boolean = {
@@ -30,24 +31,25 @@ class GuardedBlocksPlugin(val global: Global) extends Plugin {
 
     import global._
 
-    override val phaseName: String = "guardblocks"
-    override def description: String = "check for unused statements in code blocks"
-    override val runsAfter: List[String] = List("parser") //change a phase
+    override val phaseName: String = "ccplugin"
+    override def description: String = "collect sbus invocations"
+    override val runsAfter: List[String] = List("parser")
 
-    private val guardian = new BlockTreesGuardian[global.type](global)
+    private val guardian = new BlockTrees[global.type](global)
 
     override def newPhase(prev: Phase): Phase = new StdPhase(prev) {
       override def apply(unit: CompilationUnit): Unit = {
         println("********** Phase start ***********")
         val struct = guardian.getStruct(unit.body)
         println(struct.toJsonString)
-        writeFile(myOptions.get("out") + struct.name + ".json", struct)
+        writeFile(myOptions.get("out").toString, struct.name + ".json", struct)
         println("********** Phase end ***********")
       }
     }
 
-    def writeFile(filename: String, struct: Struct): Unit = {
-      val file = new File(filename)
+    def writeFile(dirName: String, filename: String, struct: Struct): Unit = {
+      new File(dirName).mkdirs();
+      val file = new File(dirName + filename)
       val bw = new BufferedWriter(new FileWriter(file))
       bw.write(struct.toJsonString)
       bw.close()
--- src/main/scala/com/copper/testplugin/components/BlockTrees.scala
+++ src/main/scala/com/copper/testplugin/components/BlockTrees.scala
@@ -5,32 +5,32 @@ import com.copper.testplugin.model.Struct
 import scala.tools.nsc.Global
 import scala.collection.mutable.ListBuffer
 
-class BlockTreesGuardian[G <: Global](val global: G) {
+class BlockTrees[G <: Global](val global: G) {
 
   import global._
   var name: String = ""
   val listOfRequests: ListBuffer[String] = ListBuffer("")
   val listOfSubscribes: ListBuffer[String] = ListBuffer("")
 
-  val guard: Tree => Unit = BlockTreesGuardianTraverser.traverse
+  val guard: Tree => Unit = BlockTreesTraverser.traverse
 
   def getStruct(tree: Tree): Struct = {
     listOfRequests.clear()
     listOfSubscribes.clear()
-    BlockTreesGuardianTraverser.traverse(tree)
+    BlockTreesTraverser.traverse(tree)
     Struct(name, listOfRequests, listOfSubscribes)
   }
 
-  private object BlockTreesGuardianTraverser extends Traverser {
+  private object BlockTreesTraverser extends Traverser {
     override def traverse(tree: Tree): Unit = {
         //println(showRaw(tree))
         name = tree.pos.source.file.name
         tree match {
           case Apply(TypeApply(Select(Ident(TermName("sbus")), TermName("request")), List(Ident(TypeName(_)))), List(Literal(Constant(value)))) => {
-            listOfRequests.append(s"sbus.request($value)")
+            listOfRequests.append(value.toString)
           }
           case Apply(Select(New(Ident(TypeName("Subscribe"))), termNames.CONSTRUCTOR), List(Literal(Constant(value)))) => {
-            listOfSubscribes.append(s"@Subscribe($value)")
+            listOfSubscribes.append(value.toString)
           }
           case _ =>
         }
