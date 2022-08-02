47890556162fe882023a9dc66395470be586f54c
cor3man <lisockij@gmail.com>
2022-08-02 11:48:16 +0200

    Create files for all classes

--- src/main/scala/com/copper/testplugin/components/BlockTrees.scala
+++ src/main/scala/com/copper/testplugin/components/BlockTrees.scala
@@ -8,53 +8,134 @@ import scala.collection.mutable.ListBuffer
 class BlockTrees[G <: Global](val global: G) {
 
   import global._
+
   var name: String = ""
-  val listOfRequests: ListBuffer[String] = ListBuffer("")
-  val listOfCommands: ListBuffer[String] = ListBuffer("")
-  val listOfEvents: ListBuffer[String] = ListBuffer("")
-  val listOfSubscribes: ListBuffer[String] = ListBuffer("")
+  var classPackage: String = ""
+
+  val listOfStructs: ListBuffer[Struct] = ListBuffer.empty
+
+  var listOfRequests: ListBuffer[String] = ListBuffer.empty
+  var listOfCommands: ListBuffer[String] = ListBuffer.empty
+  var listOfEvents: ListBuffer[String] = ListBuffer.empty
+  var listOfSubscribes: ListBuffer[String] = ListBuffer.empty
 
   val guard: Tree => Unit = BlockTreesTraverser.traverse
 
-  def getStruct(tree: Tree): Struct = {
+  def getClassFileStruct(tree: Tree): ListBuffer[Struct] = {
     listOfRequests.clear()
     listOfSubscribes.clear()
     listOfCommands.clear()
     listOfEvents.clear()
+    listOfStructs.clear()
+
+/*    def t = tree match {
+      case t @ PackageDef(Select(a, b), _) => {
+        //printWithTabs()
+        classPackage = (a + "." + b)
+        printWithTabs("PackageDef : " + classPackage, 1)
+        t
+      }
+    }*/
+
     BlockTreesTraverser.traverse(tree)
-    Struct(name, listOfRequests, listOfSubscribes, listOfCommands, listOfEvents)
+    println("structs:")
+    listOfStructs.foreach(l => println(l.toJsonString))
+    listOfStructs
   }
 
   private object BlockTreesTraverser extends Traverser {
     override def traverse(tree: Tree): Unit = {
-        //println(showRaw(tree))
-        name = tree.pos.source.file.name
-        tree match {
-/*          case t @ PackageDef(_,_) => {
-            println("--------------------------")
-            println(t.summaryString)
-            println("--------------------------")
-
-          }*/
-          case Apply(TypeApply(Select(Ident(TermName("sbus")), TermName("request")), List(_)), List(Literal(Constant(value)), _*)) => {
-            listOfRequests.append(value.toString)
-          }
-          case Apply(Select(Ident(TermName("sbus")), TermName("command")), List(Literal(Constant(value)), _*)) => {
-            listOfCommands.append(value.toString)
-          }
-          case Apply(Select(Ident(TermName("sbus")), TermName("event")), List(Literal(Constant(value)), _*)) => {
-            listOfEvents.append(value.toString)
-          }
-          case Apply(TypeApply(Select(Ident(TermName("sbus")), TermName("on")), List(_, _)), List(Literal(Constant(value)))) => {
-            listOfSubscribes.append(value.toString)
-          }
-          case Apply(Select(New(Ident(TypeName("Subscribe"))), termNames.CONSTRUCTOR), List(Literal(Constant(value)))) => {
-            listOfSubscribes.append(value.toString)
-          }
-          case _ =>
+      //printWithTabs("Start travers")
+      //println(showRaw(tree))
+      //name = tree.pos.source.file.name
+      tree match {
+        case t @ PackageDef(Select(a,b), _) => {
+          //printWithTabs()
+          classPackage = (a + "." + b)
+          printWithTabs("PackageDef : "+ classPackage, 1)
         }
+        case t @ ClassDef(_, typeName, _, _) => {
+          //
+          printWithTabs("------------ClassDef begin -------------", 1)
+          printWithTabs("++++++++++++++ " + showRaw(t))
+          val className = classPackage + "." + typeName
+          printWithTabs(classPackage + "." + typeName, 2)
+          printWithTabs(showRaw(tree), 2)
+
+          tree.children.foreach(t => printWithTabs("children  : "+ showRaw(t), 4))
+          //tree.children.foreach(ClassTraverser.traverse)
+          ClassTraverser.traverse(t)
+          def subStruct = Struct(className, listOfRequests, listOfSubscribes, listOfCommands, listOfEvents)
+          printWithTabs(subStruct.toJsonString, 3)
+
+          listOfStructs.append(subStruct)
+
+          listOfSubscribes = ListBuffer.empty
+          listOfRequests = ListBuffer.empty
+          listOfCommands = ListBuffer.empty
+          listOfEvents = ListBuffer.empty
+
+          printWithTabs("------------ClassDef end -------------", 1)
+        }
+        case ModuleDef(_, typeName, _) => {
+          printWithTabs("------------S - ModuleDef begin -------------", 1)
+
+          val objectName = classPackage + "." + typeName
+          printWithTabs(classPackage + "." + typeName, 2)
+          printWithTabs(showRaw(tree), 2)
+          //println(t)
+          tree.children.foreach(ClassTraverser.traverse)
+          def subStruct = Struct(objectName, listOfRequests, listOfSubscribes, listOfCommands, listOfEvents)
+
+          listOfStructs.append(subStruct)
+
+          listOfSubscribes = ListBuffer.empty
+          listOfRequests = ListBuffer.empty
+          listOfCommands = ListBuffer.empty
+          listOfEvents = ListBuffer.empty
+
+          printWithTabs("------------E - ModuleDef end -------------", 1)
+        }
+        case _ =>
+      }
       super.traverse(tree)
     }
+  }
+
+  private object ClassTraverser extends Traverser {
+    override def traverse(tree: Tree): Unit = {
+      name = tree.pos.source.file.name
+      tree match {
+        case Apply(TypeApply(Select(Ident(TermName("sbus")), TermName("request")), List(_*)), List(Literal(Constant(value)))) => {
+          printWithTabs(showRaw(tree), 3)
+          listOfRequests.append(value.toString)
+        }
+        case Apply(Select(Ident(TermName("sbus")), TermName("command")), List(Literal(Constant(value)), _*)) => {
+          printWithTabs(showRaw(tree), 3)
+          listOfCommands.append(value.toString)
+        }
+        case Apply(Select(Ident(TermName("sbus")), TermName("event")), List(Literal(Constant(value)), _*)) => {
+          printWithTabs(showRaw(tree), 3)
+          listOfEvents.append(value.toString)
+        }
+        case Apply(TypeApply(Select(Ident(TermName("sbus")), TermName("on")), List(_, _)), List(Literal(Constant(value)))) => {
+          printWithTabs(showRaw(tree), 3)
+          listOfSubscribes.append(value.toString)
+        }
+        case Apply(Select(New(Ident(TypeName("Subscribe"))), termNames.CONSTRUCTOR), List(Literal(Constant(value)))) => {
+          printWithTabs(showRaw(tree), 3)
+          listOfSubscribes.append(value.toString)
+        }
+        case _ =>
+      }
+      super.traverse(tree)
+    }
+
+  }
+
+  def printWithTabs(s: String, tabs: Int = 0): Unit = {
+    //(1 to tabs).foreach(_ => print("-" + _))
+    println(("  " * tabs) + s)
   }
 }
 
